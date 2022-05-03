5e90c0d071e259b38b68abb73656a9a66f65a1eb
Urishita Pandita <67178658+urishitapandita@users.noreply.github.com>
2022-05-03 22:33:46 +0530

    Add files via upload

---
+++ Challenge/Arrays.cpp/75.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int i = 0;
+        int j = 0;
+        int k = nums.size() - 1;
+        while (j <= k)
+        {
+            if (nums[j] == 1)
+            {
+                j++;
+            }
+            else if (nums[j] == 0)
+            {
+                swap(nums[j], nums[i]);
+                i++;
+                j++;
+            }
+            else
+            {
+                swap(nums[j], nums[k]);
+                k--;
+            }
+        }
+        return;
+    }
+};
