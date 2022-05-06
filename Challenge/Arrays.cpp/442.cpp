55b0cc3c9a994e43255821bcd60190ef0591390e
Urishita Pandita <67178658+urishitapandita@users.noreply.github.com>
2022-05-06 23:51:31 +0530

    Add files via upload

---
+++ Challenge/Arrays.cpp/442.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> findDuplicates(vector<int> &nums)
+    {
+        vector<int> ans;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            int idx = abs(nums[i]) - 1;
+
+            if (nums[idx] < 0)
+            {
+                ans.push_back(idx + 1);
+            }
+
+            else
+            {
+                nums[idx] *= -1;
+            }
+        }
+
+        return ans;
+    }
+};
